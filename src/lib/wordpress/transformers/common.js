76788e9003c809da422c91036b3ad73bee79b6f1
Kerolos Amiel <kerolosamiel4@gmail.com>
2026-09-11 14:32:33 +0300

    feat(wordpress): add common transformers for SEO metadata and image normalization

---
+++ src/lib/wordpress/transformers/common.js
@@ -0,0 +1,44 @@
+// Image Object Details
+export function imageTransform(imageObject) {
+  if (imageObject == null) return '';
+
+  if (typeof imageObject == String)
+    return {
+      url: imageObject,
+      alt: '',
+    };
+
+  const image = {
+    url: imageObject.url || '',
+    alt: imageObject.alt || '',
+  };
+
+  return image;
+}
+
+// SEO Meta Details
+export function metaTransform(pageData) {
+  if (!pageData) return null;
+
+  if (!pageData.acf_all_fields) return null;
+
+  if (!pageData.acf_all_fields.meta_details) return null;
+
+  const metaDetails = pageData.acf_all_fields.meta_details;
+
+  // Main values
+  const title = metaDetails.meta_title || pageData.title || pageData.title?.rendered || '';
+  const description = metaDetails.meta_description || '';
+
+  const meta = {
+    title,
+    description,
+    keywords: metaDetails.meta_keywords || 'Scalezone, Amazon, Sellers',
+    ogImage: metaDetails.og_image || '',
+    ogTitle: metaDetails.og_title || title,
+    ogDescription: metaDetails.og_description || description,
+  };
+  console.log(meta);
+
+  return meta;
+}
