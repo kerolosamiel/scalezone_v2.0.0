1ea79f0f369ee426753bd9a52924292a99af9c63
Kerolos Amiel <kerolosamiel4@gmail.com>
2026-09-13 17:15:56 +0300

    fix: remove test codes from the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,7 @@
-import TestimonialCard from '@/components/sections/Testimonials/TestimonialCard';
-import { getHomePage } from '@/lib/wordpress/data-fetching/queries';
-
-export default async function Home() {
-  const data = await getHomePage();
-
-  console.log(data);
+export default function Home() {
   return (
     <main>
       <h1>Home</h1>
-
-      <TestimonialCard testimonial={data?.testimonials?.global?.testimonials[3]} />
     </main>
   );
 }
