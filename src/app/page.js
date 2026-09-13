c75259f5b4cb43abca5602e3017251eefa313dd1
Kerolos Amiel <kerolosamiel4@gmail.com>
2026-09-13 14:27:15 +0300

    fix: remove all test lines from home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,4 @@
-import { getAboutPage } from '@/lib/wordpress/data-fetching/queries';
-
 export default async function Home() {
-  const data = await getAboutPage();
-  console.log(data);
   return (
     <main>
       <h1>Home</h1>
