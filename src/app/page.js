a0224af00500af960da6b29436554fc6b954ffa3
Kerolos Amiel <kerolosamiel4@gmail.com>
2026-09-14 17:25:25 +0300

    fix: remove the test line from home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,7 @@
-import FormChip from '@/components/ui/formchip';
-
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
-
-      <FormChip>Amazon Services</FormChip>
     </main>
   );
 }
