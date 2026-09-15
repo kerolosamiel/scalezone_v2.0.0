4ff0f8fb5d68f4cf781c695fd53a7f4a0a37b5f1
Kerolos Amiel <kerolosamiel4@gmail.com>
2026-09-15 18:52:19 +0300

    feat(app): add 404 Not Found page with custom styling and SEO metadata

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,3 +1,33 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
+export const metadata = {
+  title: '404 - Page Not Found | Scalezone',
+  description: 'The page you are looking for does not exist.',
+  robots: {
+    index: false,
+    follow: true,
+  },
+};
+
 export default function NotFound() {
-  return '';
+  return (
+    <main>
+      <section className="h-[calc(100dvh-6.4rem)] flex flex-col justify-center items-center my-32 gap-32 text-center">
+        <div>
+          <h1 className="text-[21rem] tracking-[6%]">404</h1>
+          <h2 className="text-[1.6rem] tracking-[6%]">Page Not Found</h2>
+        </div>
+
+        <div>
+          <p className="text-[1.6rem] max-w-650 mb-24 text-muted-foreground">
+            Sorry, the page you are looking for doesn't exist or has been moved.
+          </p>
+          <Button asChild className="text-[1.6rem] h-fit w-fit py-16 px-30">
+            <Link href="/">Back To Home</Link>
+          </Button>
+        </div>
+      </section>
+    </main>
+  );
 }
