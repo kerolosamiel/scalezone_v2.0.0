df36a605fd629ad521ddd1cd57a494ab1980e89c
Kerolos Amiel <kerolosamiel4@gmail.com>
2026-09-16 14:28:46 +0300

    feat: change page colors and button hover

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -13,17 +13,20 @@ export const metadata = {
 export default function NotFound() {
   return (
     <main>
-      <section className="h-[calc(100dvh-6.4rem)] flex flex-col justify-center items-center my-32 gap-32 text-center">
+      <section className="h-[calc(100dvh-6.4rem)] px-16 flex flex-col justify-center items-center my-32 gap-32 text-center">
         <div>
-          <h1 className="text-[21rem] tracking-[6%]">404</h1>
+          <h1 className="text-[21rem] max-sm:text-[14rem] tracking-[6%] text-primary">404</h1>
           <h2 className="text-[1.6rem] tracking-[6%]">Page Not Found</h2>
         </div>
 
         <div>
-          <p className="text-[1.6rem] max-w-650 mb-24 text-muted-foreground">
+          <p className="text-[1.6rem] max-sm:text-[1.4rem] max-w-650 mb-24 text-muted-foreground">
             Sorry, the page you are looking for doesn't exist or has been moved.
           </p>
-          <Button asChild className="text-[1.6rem] h-fit w-fit py-16 px-30">
+          <Button
+            asChild
+            className="text-[1.6rem] h-fit w-fit py-16 px-30 hover:bg-button-hover-gradient hover:scale-[1.1] transition-all duration-500"
+          >
             <Link href="/">Back To Home</Link>
           </Button>
         </div>
