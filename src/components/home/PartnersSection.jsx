08054a1a98740434d43c91a58669f1436f0ad376
Kerolos Amiel <kerolosamiel4@gmail.com>
2026-09-22 14:19:13 +0300

    refactor(Partners): make the component returns null if the arr is empty, so never add the section to the page

--- src/components/home/PartnersSection.jsx
+++ src/components/home/PartnersSection.jsx
@@ -1,11 +1,11 @@
 import Image from 'next/image';
 
 export default function PartnersSection({ partners }) {
-  if (!partners) return null;
+  if (!partners || partners.length == 0) return null;
 
   return (
     <section className="py-32 px-64 border border-x-0 flex gap-128 flex-wrap justify-center max-xl:gap-48 max-lg:px-32">
-      {partners.map((partner) => (
+      {partners?.map((partner) => (
         <div className="relative w-xs h-100" key={partner?.id}>
           <Image
             src={partner?.url}
